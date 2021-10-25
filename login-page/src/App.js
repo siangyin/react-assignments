0966caf26b460e2d62a95cdf2a1c590d890e371e
Lee Siang Yin <siangyin916@gmail.com>
2021-10-25 17:43:16 +0800

    useEffect

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,31 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+	const loginHandler = (email, password) => {
+		// We should of course check email and password
+		// But it's just a dummy/ demo anyways
+		localStorage.setItem("isLoggedIn", "1");
+		setIsLoggedIn(true);
+	};
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+	const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+	// useEffect hook with callBack and empty array of dependencies will be called: on initial render (aka mounting-phase). Only run once.
+
+	useEffect(() => {
+		if (storedUserLoggedInInfo === "1") {
+			setIsLoggedIn(true);
+		}
+	}, []);
+
+	const logoutHandler = () => {
+		setIsLoggedIn(false);
+		localStorage.setItem("isLoggedIn", "0");
+	};
+
+	return (
+		<React.Fragment>
+			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+			<main>
+				{!isLoggedIn && <Login onLogin={loginHandler} />}
+				{isLoggedIn && <Home onLogout={logoutHandler} />}
+			</main>
+		</React.Fragment>
+	);
 }
 
 export default App;
--- login-page/src/components/Login/Login.js
+++ login-page/src/components/Login/Login.js
@@ -1,84 +1,115 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+console.log("loaded page runnning js file.");
+
+const emailReducer = (state, action) => {
+	return { value: "", isValid: false };
+};
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [formIsValid, setFormIsValid] = useState(false);
+	// const [enteredEmail, setEnteredEmail] = useState("");
+	// const [emailIsValid, setEmailIsValid] = useState();
+	const [enteredPassword, setEnteredPassword] = useState("");
+	const [passwordIsValid, setPasswordIsValid] = useState();
+	const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+	const [emailState, dispatchEmail] = useReducer(emailReducer);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
-  };
+	// useEffect(() => {
+	// 	console.log("empty arr useEffect run only once, first mounted");
+	// 	return () => {
+	// 		console.log(
+	// 			"empty arr useEffect : cleanup run when component is removed from DOM"
+	// 		);
+	// 	};
+	// }, []);
 
-  const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+	// useEffect(() => {
+	// 	// useEffect hook with callBack and array of dependencies will be called: on initial render (aka mounting-phase) and when any value of the dependencies change.
+	// 	// console.log("check form validy");
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
-  };
+	// 	// running checking only after 1s (debouncing)
+	// 	const identifier = setTimeout(() => {
+	// 		// console.log("checking now");
+	// 		setFormIsValid(enteredPassword.trim().length > 6 && emailIsValid);
+	// 	}, 1000);
 
-  const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
-  };
+	// 	// a clean up function to run before every sideEffect timeout function being execute and does not run before the very first execution, so we only have one timer at a time. We clear timer before new start.
+	// 	return () => {
+	// 		// console.log("cleanup now");
+	// 		clearTimeout(identifier);
+	// 	};
+	// }, [enteredEmail, enteredPassword]);
 
-  const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
-  };
+	const emailChangeHandler = (event) => {
+		setFormIsValid(emailState.isValid && enteredPassword.trim().length > 6);
+	};
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
-  };
+	const passwordChangeHandler = (event) => {
+		setEnteredPassword(event.target.value);
 
-  return (
-    <Card className={classes.login}>
-      <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={enteredPassword}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
-        <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
-            Login
-          </Button>
-        </div>
-      </form>
-    </Card>
-  );
+		setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+	};
+
+	const validateEmailHandler = () => {
+		//validateEmail check if valid email then set email.
+		let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if (emailState.value.match(regexEmail)) {
+		}
+	};
+
+	const validatePasswordHandler = () => {
+		setPasswordIsValid(enteredPassword.trim().length > 6);
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		props.onLogin(emailState.value, enteredPassword);
+	};
+
+	return (
+		<Card className={classes.login}>
+			<form onSubmit={submitHandler}>
+				<div
+					className={`${classes.control} ${
+						emailState.isValid === false ? classes.invalid : ""
+					}`}
+				>
+					<label htmlFor="email">E-Mail</label>
+					<input
+						type="email"
+						id="email"
+						value={emailState.value}
+						onChange={emailChangeHandler}
+						onBlur={validateEmailHandler}
+					/>
+				</div>
+				<div
+					className={`${classes.control} ${
+						passwordIsValid === false ? classes.invalid : ""
+					}`}
+				>
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						id="password"
+						value={enteredPassword}
+						onChange={passwordChangeHandler}
+						onBlur={validatePasswordHandler}
+					/>
+				</div>
+				<div className={classes.actions}>
+					<Button type="submit" className={classes.btn} disabled={!formIsValid}>
+						Login
+					</Button>
+				</div>
+			</form>
+		</Card>
+	);
 };
 
 export default Login;
