ae122e033a5193866403ad75aecb7e9a5836e0c1
Lee Siang Yin <siangyin916@gmail.com>
2021-10-27 10:58:17 +0800

    export data in js

--- e-shop/src/index.js
+++ e-shop/src/index.js
@@ -1,29 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import itemArr from "./itemArr";
 
 function ProductList() {
 	return (
 		<section className="productlist">
-			<Product />
+			{itemArr.map((item) => {
+				return <Product key={item.code} {...item} />;
+			})}
 			<Product />
 		</section>
 	);
 }
 
-function Product() {
+function Product(props) {
 	return (
-		<article className="product">
-			<img
-				src="https://cdn.shopify.com/s/files/1/0978/5112/products/PRO-2100_580x580_02_1_800x.png?v=1593188353"
-				alt="pro-2100"
-			></img>
-			<h1>Pro-521</h1>
+		<article
+			className="product"
+			onMouseOver={() => {
+				console.log(props.model);
+			}}
+		>
+			<img src={props.src} alt={props.model}></img>
+
+			<h1>{props.model}</h1>
 			<h4>
-				12-colour 24" Professional Photographic Large-Format Inkjet Printer with
-				Multifunction Roll System
+				{props.series}: {props.color}
 			</h4>
+
+			<h4>
+				Price: US$
+				{props.price}
+			</h4>
+			<button
+				onClick={() => {
+					console.log(props.price);
+				}}
+			>
+				Add to Cart
+			</button>
 		</article>
 	);
 }
