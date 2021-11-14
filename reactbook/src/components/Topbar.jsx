be2dd2e6189a46bb55986f9d62a067e2facfba4f
Lee Siang Yin <siangyin916@gmail.com>
2021-11-14 18:18:41 +0800

    creating topbar

--- reactbook/src/components/Topbar.jsx
+++ reactbook/src/components/Topbar.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React from "react";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 
 const Topbar = () => {
- return (
-  <div className="topbarContainer">
-   
-   <div className="topbarLeft">
+	return (
+		<div className="topbarContainer">
+			<div className="topbarLeft">
+				<span className="logo">ReactBook</span>
+			</div>
 
-    
-   </div>
- </div>
- )
-}
+			<div className="topbarCenter">
+				<div className="searchbar">
+					<Search className="searchIcon" />
+					<input placeholder="search" className="searchInput" />
+				</div>
+			</div>
 
-export default Topbar
+			<div className="topbarRight">
+				<span className="topbarLink">Homepage</span>
+				<span className="topbarLink">Timeline</span>
+			</div>
+
+			<div className="topbarIcons">
+				<div className="topbarIconItem">
+					<Person />
+					<span className="topbarIconBadge">1</span>
+				</div>
+
+				<div className="topbarIconItem">
+					<Chat />
+					<span className="topbarIconBadge">2</span>
+				</div>
+
+				<div className="topbarIconItem">
+					<Notifications />
+					<span className="topbarIconBadge">3</span>
+				</div>
+
+				<img
+					src="../../public/assets/potrait01.jpeg"
+					alt="img"
+					className="topbarImg"
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Topbar;
